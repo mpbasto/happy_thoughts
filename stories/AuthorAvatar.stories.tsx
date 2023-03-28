8a4a0940ccd46a28381a291abbe175308c5d4dbc
Marta Basto <87577613+mpbasto@users.noreply.github.com>
2023-03-28 18:20:16 +0100

    Add new OG image, add OriginalNextImage to preview.js

--- stories/AuthorAvatar.stories.tsx
+++ stories/AuthorAvatar.stories.tsx
@@ -16,7 +16,6 @@ export default {
   },
 } as ComponentMeta<typeof Avatar>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
 
 export const Primary = Template.bind({});
